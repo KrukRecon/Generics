77e514f683ca9b0887186d942b3478ca19b70a86
Adam <adam.kruk@sdworx.com>
2022-02-16 14:30:31 +0100

    Using generics with simple classes

---
+++ StackClass/StackClass/SimpleStack.cs
@@ -0,0 +1,16 @@
+﻿namespace StackClass
+{
+    public class SimpleStack<T>
+    {
+        private readonly T[] _items;
+        private int _currentIndex = -1;
+
+        public SimpleStack() => _items = new T[10];
+
+        public void Push(T item) => _items[++_currentIndex] = item;
+
+        public int Count => _currentIndex + 1;
+
+        public T Pop() => _items[_currentIndex--];
+    }
+}
